36dd81bf3dface73ef5800e011be697bbc7cd7f2
Camleo1011 <leonmonsalvecamilojose@gmail.com>
2026-03-22 14:57:47 -0500

    agregando un mensaje en cada iteracion

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -15,10 +15,10 @@ namespace Fibo
 
         static void Fibonacci()
         {
-            int a;
-            int b;
-            int c;
-            int con = 2;
+            long a;
+            long b;
+            long c;
+            long con = 2;
             long lim;
             bool limValido;
 
@@ -32,36 +32,36 @@ namespace Fibo
             }while(limValido == false || lim < 0);
 
             a = 0;
-
-            b = 0;
             b = 1;
             
             if (lim == 0)
             {
-                Console.WriteLine(a);
+                Console.WriteLine($"estamos en la posicion: {con-2} y el  numero es: {a}");
                 return;
             }
             else if (lim == 1)
             {
-                Console.WriteLine(a);
-                Console.WriteLine(b);
+                Console.WriteLine($"estamos en la posicion: {con-2} y el  numero es: {a}");
+                Console.WriteLine($"estamos en la posicion: {con-1} y el  numero es: {b}");
+
                 return;
             }
             else
             {
-                Console.WriteLine(a);
-                Console.WriteLine(b);
+                Console.WriteLine($"estamos en la posicion: {con-2} y el  numero es: {a}");
+                Console.WriteLine($"estamos en la posicion: {con-1} y el  numero es: {b}");
                 do
                 {
                     c = a + b;
 
                     a = b;
                     b = c;
+                    Console.WriteLine($"estamos en la posicion: {con} y el  numero es: {c}");
                     con = con + 1;
-                    Console.WriteLine(c);
+                    
                 } while (con <= lim);
             }
-            
+           
         }
     }
 }
